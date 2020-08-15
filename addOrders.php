e2fefdb291714aa4a5b01402e1f5706bf3157b6e
Bhabalomkar421 <Omkarbhabal11@gmail.com>
2020-08-15 12:52:43 +0530

    final changes done

--- addOrders.php
+++ addOrders.php
@@ -1,20 +1,29 @@
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
 <?php
     include('nav.php');
     include('db.php');
-    // $delete_query = "delete from cart";
-    // if(mysqli_query($con, $delete_query)){
-    //     echo "";
-    // }else{
-    //     echo "";
-    // }
+    $select_customers = "select * from customer";
+    $run_cust_query = mysqli_query($con, $select_customers);
+     
 ?>
 
 <div class="container my-4">
 <div id="error-message" class="alert alert-warning" style="display:none;"></div>
 <div id="success-message" class="alert alert-success" style="display:none;"></div>
 <form id="addForm">
-    <label for='customer_id'>customer Id</label>
-    <input type="fixed" class="form-control" name="customer_id" id = "customer_id" required>
+    <label for='customer_id'>Customer </label>
+    <select id="customer_id" name='customer_id' class="form-control">
+    <?php
+        // $i = 0;
+        while($customer = mysqli_fetch_array($run_cust_query)) {
+            $customer_name = $customer['customer_name'];
+            $customer_id = $customer['id'];
+            echo "<option id='$customer_id' value='$customer_id'>$customer_name</option>";
+            // $i++;
+        }
+    ?>
+    </select>
     <label for='product_id'>Product Id</label>
     <input type="text" class="form-control" name="product_id" id = "product_id" required>
     <label for='quantity'>quantity </label>
@@ -41,10 +50,12 @@ $(document).ready(function(){
     }
     loadTable();
 
+   
     $("#save-button").on("click",function(e){
         e.preventDefault();
-        var cust_id = $("#customer_id").val();
-        var prod_id = $("#product_id").val();
+
+        var cust_id = $( "#customer_id" ).val();
+        var prod_id = $( "#product_id" ).val();
         var quan = $("#quantity").val();
         if(cust_id == "" || prod_id == "" || quan == ""){
             $("#error-message").html("All fields are required").slideDown();
--- deleteCustomer.php
+++ deleteCustomer.php
@@ -1,17 +1,40 @@
 <?php
     include('db.php');
     $customer_id = $_GET['id'];
+    
+    $select_order = "select * from orders where customer_id = $customer_id";
+    $run_select_order = mysqli_query($con, $select_order);
+    while($rows_x = mysqli_fetch_array($run_select_order)){
+        $product_id = $rows_x['product_id'];
+        $quantity = $rows_x['quantity'];
+        
+        $select_quantity = "select product_quantity from products where product_id = $product_id";
+        $run_select_query2 = mysqli_query($con, $select_quantity);
+        while($rows1 = mysqli_fetch_array($run_select_query2)){
+            $product_quantity = $rows1['product_quantity'];
+        }
+    
+        $final_quantity = $quantity + $product_quantity;
+        $update_query = "UPDATE products SET product_quantity = '$final_quantity' where product_id = $product_id";
+        if(mysqli_query($con, $update_query)){
+            echo ".";
+        } else {
+            echo "Error updating record: ".mysqli_error($con);
+        }
+    }
+
     $delete_query = "delete from customer where id = $customer_id";
     $delete_order = "delete from orders where customer_id = $customer_id";
     if(mysqli_query($con, $delete_order)){
-        if(mysqli_query($con, $delete_query)){
+        if(mysqli_query($con, $delete_query) ){
             echo "<script>window.open('viewCustomers.php','_self')</script>";
         }else{
             echo "";
         }
     }else{
-        echo "can't delete account";
+        echo ".";
     }
+    
     
      
 
