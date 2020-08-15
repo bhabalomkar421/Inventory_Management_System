9f173af1cec4f247b25b8b712d0a8df10ffade9d
Bhabalomkar421 <Omkarbhabal11@gmail.com>
2020-08-15 11:06:14 +0530

    compulsory changes done

--- deleteCustomer.php
+++ deleteCustomer.php
@@ -1,12 +1,18 @@
 <?php
     include('db.php');
-    $customer_id = $_GET['id']; 
+    $customer_id = $_GET['id'];
     $delete_query = "delete from customer where id = $customer_id";
-    if(mysqli_query($con, $delete_query)){
-        echo "<script>window.open('viewCustomers.php','_self')</script>";
+    $delete_order = "delete from orders where customer_id = $customer_id";
+    if(mysqli_query($con, $delete_order)){
+        if(mysqli_query($con, $delete_query)){
+            echo "<script>window.open('viewCustomers.php','_self')</script>";
+        }else{
+            echo "";
+        }
     }else{
-        echo "";
+        echo "can't delete account";
     }
+    
      
 
 ?>
